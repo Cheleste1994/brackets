1e52d6537a2ed64e855177eb5b541ada1aa79de5
Cheleste1994 <wolverines@tut.by>
2023-02-22 17:14:42 +0300

    refactor: task solved

--- src/index.js
+++ src/index.js
@@ -12,3 +12,18 @@ if (str.length === 0) {
   return false;
 }
 }
+
+
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+let check = '([{}])';
+
+for (let i = 0; i < config3.length;)
+  if (!check.includes(config3[i].join(''))) {
+      i++;
+      console.log(check)
+  } else {
+      check = check.replace(config3[i].join(''), '');
+      i = 0;
+  }
+
+console.log(check)
